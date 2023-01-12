d270871f758df3d09899a7fc30dd69678dd0991b
Shishir-Jakhmola <shishir.jakhmola@outlook.com>
2023-01-12 12:04:26 +0530

    CRUD implemented

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,40 +1,53 @@
-import { useState, useRef } from 'react';
-import { TasksListComponent } from './TaskList'
-import './ToDo.css';
+import { useState, useRef } from "react";
+import "./ToDo.css";
 
 function ToDo() {
-  let [TasksStateVar, TaskUpdater] = useState([])
+  let [Tasks, SetTasks] = useState([]);
+  let [EditingTaskId, SetEditingTaskId] = useState('');
+  let [EditingTaskTitle, SetEditingTaskTitle] = useState('');
   let TaskTitle = useRef();
   return (
     <div className="todo">
-
       <h1>ToDo App</h1>
 
-      <form id="ToDoForm" onSubmit={(e) => {
-        e.preventDefault();
-        // TasksStateVar.push({
-        //   Id: TasksStateVar.length,
-        //   TaskTitle: TaskTitle.current.value
-        // });
-        console.log(TasksStateVar);
-        TaskUpdater(
-          [
-          ...TasksStateVar,
-          {
-          Id: TasksStateVar.length,
-          TaskTitle: TaskTitle.current.value
-        }
-      ]
-        );
-        TaskTitle.current.value = "";
-        }}>
+      <form
+        id="ToDoForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          SetTasks([
+            ...Tasks,
+            {
+              Id: Tasks.length,
+              TaskTitle: TaskTitle.current.value,
+            },
+          ]);
+          TaskTitle.current.value = "";
+        }}
+      >
         <label htmlFor="title">Task Title </label>
         <input type="text" id="title" ref={TaskTitle}></input>
         <input type="submit"></input>
       </form>
 
       <ol>
-      <TasksListComponent TasksList={TasksStateVar} />
+        {Tasks.map((task) => {
+          return (
+            <li key={task.Id}>
+              {EditingTaskId === task.Id ? <input type="text" value={EditingTaskTitle} onChange={(e) => {SetEditingTaskTitle(e.target.value)}}></input> : task.TaskTitle}
+              <button onClick={() => {SetEditingTaskId(task.Id);SetEditingTaskTitle(task.TaskTitle)}}>Edit</button>
+              <button onClick={() => {let UpdatedTasks = Tasks.map((task) => {if (task.Id === EditingTaskId) {return {
+                Id: EditingTaskId,
+                TaskTitle: EditingTaskTitle,
+              };
+              };
+              return task;});
+              SetTasks(UpdatedTasks);
+              SetEditingTaskId('');
+              SetEditingTaskTitle('');
+              }}>Save</button>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
