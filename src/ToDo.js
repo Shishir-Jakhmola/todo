c3280df16ad241f8d66160baab650db28b266efd
Shishir-Jakhmola <shishir.jakhmola@outlook.com>
2023-01-12 12:04:59 +0530

    Formatted code

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,8 +3,8 @@ import "./ToDo.css";
 
 function ToDo() {
   let [Tasks, SetTasks] = useState([]);
-  let [EditingTaskId, SetEditingTaskId] = useState('');
-  let [EditingTaskTitle, SetEditingTaskTitle] = useState('');
+  let [EditingTaskId, SetEditingTaskId] = useState("");
+  let [EditingTaskTitle, SetEditingTaskTitle] = useState("");
   let TaskTitle = useRef();
   return (
     <div className="todo">
@@ -33,18 +33,43 @@ function ToDo() {
         {Tasks.map((task) => {
           return (
             <li key={task.Id}>
-              {EditingTaskId === task.Id ? <input type="text" value={EditingTaskTitle} onChange={(e) => {SetEditingTaskTitle(e.target.value)}}></input> : task.TaskTitle}
-              <button onClick={() => {SetEditingTaskId(task.Id);SetEditingTaskTitle(task.TaskTitle)}}>Edit</button>
-              <button onClick={() => {let UpdatedTasks = Tasks.map((task) => {if (task.Id === EditingTaskId) {return {
-                Id: EditingTaskId,
-                TaskTitle: EditingTaskTitle,
-              };
-              };
-              return task;});
-              SetTasks(UpdatedTasks);
-              SetEditingTaskId('');
-              SetEditingTaskTitle('');
-              }}>Save</button>
+              {EditingTaskId === task.Id ? (
+                <input
+                  type="text"
+                  value={EditingTaskTitle}
+                  onChange={(e) => {
+                    SetEditingTaskTitle(e.target.value);
+                  }}
+                ></input>
+              ) : (
+                task.TaskTitle
+              )}
+              <button
+                onClick={() => {
+                  SetEditingTaskId(task.Id);
+                  SetEditingTaskTitle(task.TaskTitle);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  let UpdatedTasks = Tasks.map((task) => {
+                    if (task.Id === EditingTaskId) {
+                      return {
+                        Id: EditingTaskId,
+                        TaskTitle: EditingTaskTitle,
+                      };
+                    }
+                    return task;
+                  });
+                  SetTasks(UpdatedTasks);
+                  SetEditingTaskId("");
+                  SetEditingTaskTitle("");
+                }}
+              >
+                Save
+              </button>
             </li>
           );
         })}
